70561c519d2b2dddd76cb49d355e2efec7f6bf22
Barney Carroll <barney.carroll@gmail.com>
2017-10-26 12:09:49 +0100

    Remove flaky sticky polyfill dependency, use home-grown

--- app/src/assets/js/app.js
+++ app/src/assets/js/app.js
@@ -20,7 +20,25 @@ require('./libs/jquery.waitforimages');
 
 require('./libs/css3-animate-it');
 
-require('./components/_sticky');
+// Simple sticky polyfill:
+// Use a hidden 'relative' clone to allow sibling layout computations
+// Use a visible 'fixed' clone to appear in the desired position
+void function stickyNav(){
+  var $original    = document.querySelector('.navbar')
+  var $replacement = document.createDocumentFragment()
+  var $fixed       = $replacement.appendChild($original.cloneNode(true))
+  var $ghost       = $replacement.appendChild($original.cloneNode(true))
+
+  $fixed.style.position   = 'fixed'
+  $fixed.style.width      = '100%'
+
+  $ghost.style.position   = 'relative'
+  $ghost.style.visibility = 'hidden'
+  $ghost.tabIndex         = -1
+  $ghost.setAttribute('aria-hidden', true)
+
+  $original.parentNode.replaceChild($replacement, $original)
+}()
 
 require('./functions');
 
@@ -51,8 +69,6 @@ require('./components/_parallex');
 require('./components/_contact');
 
 require('./components/_knob');
-
-require('./components/_navFixedOnScroll');
 
 require('./components/_map');
 
